fdb7488233bf065032f3623342df207d45bb9f6c
ZurabDz <zuradzindzibadze06@gmail.com>
2023-01-03 21:13:20 +0400

    deepspeed + gradient clipping

--- train.py
+++ train.py
@@ -1,11 +1,18 @@
 from datamodule import CommonVoiceDataModule
 from model import MiniatureVoice
 import pytorch_lightning as pl
+from pytorch_lightning.strategies import DeepSpeedStrategy
 
 
 model = MiniatureVoice()
 data_module = CommonVoiceDataModule()
 
 
-trainer = pl.Trainer(accelerator='gpu')
+trainer = pl.Trainer(accelerator='gpu', gradient_clip_val=1, max_epochs=50, 
+# strategy=DeepSpeedStrategy(
+        # stage=3,
+        # offload_optimizer=True,
+        # offload_parameters=True,
+    # ),
+)
 trainer.fit(model, datamodule=data_module)
