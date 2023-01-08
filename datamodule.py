a1dc5e4267b3bc1ef838669f3611d01fab9ca98f
ZurabDz <zuradzindzibadze06@gmail.com>
2023-01-08 15:43:10 +0400

    added args

--- datamodule.py
+++ datamodule.py
@@ -2,14 +2,18 @@ import pytorch_lightning as pl
 from dataset import CommonVoiceDataset
 from torch.utils.data import DataLoader
 from dataset import custom_collate_fn
+from typing import Union, Optional
+from pathlib import Path
 
 
 class CommonVoiceDataModule(pl.LightningDataModule):
-    def __init__(self):
+    def __init__(self, root_dir: Union[str, Path], batch_size: Optional[int] = 4, num_workers: Optional[int] = 6):
         super().__init__()
-        self.common_voice_train = CommonVoiceDataset('data/cv-corpus-12.0-2022-12-07/ka/', 'train')
-        self.common_voice_valid = CommonVoiceDataset('data/cv-corpus-12.0-2022-12-07/ka/', 'dev') 
-        self.common_voice_test = CommonVoiceDataset('data/cv-corpus-12.0-2022-12-07/ka/', 'test')
+        if isinstance(root_dir, Path):
+            self.root_dir = str(root_dir)
+        self.common_voice_train = CommonVoiceDataset(root_dir, 'train')
+        self.common_voice_valid = CommonVoiceDataset(root_dir, 'dev') 
+        self.common_voice_test = CommonVoiceDataset(root_dir, 'test')
 
     def train_dataloader(self):
         return DataLoader(self.common_voice_train, batch_size=4,num_workers=6, collate_fn=custom_collate_fn, shuffle=True)
