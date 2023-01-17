4b35bd03ebd2317f6eb4f6acff5d7bdf3a638e7e
ZurabDz <zuradzindzibadze06@gmail.com>
2023-01-17 21:34:41 +0400

    some optimizations

--- train.py
+++ train.py
@@ -2,17 +2,24 @@ from datamodule import CommonVoiceDataModule
 from model import MiniatureVoice
 import pytorch_lightning as pl
 from pytorch_lightning.strategies import DeepSpeedStrategy
+import composer.functional as cf
+from pytorch_lightning.callbacks import ModelPruning
 
 
 model = MiniatureVoice()
-data_module = CommonVoiceDataModule('data/cv-corpus-12.0-2022-12-07/ka')
+
+model = cf.apply_squeeze_excite(model)
 
 
-trainer = pl.Trainer(accelerator='gpu', gradient_clip_val=1, max_epochs=50, precision=16
+data_module = CommonVoiceDataModule('data/cv-corpus-12.0-2022-12-07/ka', batch_size=4)
+
+
+trainer = pl.Trainer(accelerator='gpu', gradient_clip_val=1, max_epochs=50, precision=16, check_val_every_n_epoch=2,
+accumulate_grad_batches=12, strategy='deepspeed'
 # strategy=DeepSpeedStrategy(
-        # stage=3,
-        # offload_optimizer=True,
-        # offload_parameters=True,
-    # ),
+#         stage=3,
+#         offload_optimizer=True,
+#         offload_parameters=True,
+#     ),
 )
 trainer.fit(model, datamodule=data_module)
