65099513e97b858f927744eb69b79a697834f189
ZurabDz <zuradzindzibadze06@gmail.com>
2023-01-22 16:00:09 +0400

    new collator usage

--- miniature_voice/datamodule.py
+++ miniature_voice/datamodule.py
@@ -1,13 +1,13 @@
 import pytorch_lightning as pl
 from .dataset import CommonVoiceDataset
 from torch.utils.data import DataLoader
-from .dataset import custom_collate_fn
 from typing import Union, Optional
 from pathlib import Path
+from .collate import MyCollator
 
 
 class CommonVoiceDataModule(pl.LightningDataModule):
-    def __init__(self, root_dir: Union[str, Path], batch_size: Optional[int] = 4, num_workers: Optional[int] = 6):
+    def __init__(self, root_dir: Union[str, Path], batch_size: Optional[int] = 4, num_workers: Optional[int] = 2):
         super().__init__()
         if isinstance(root_dir, Path):
             self.root_dir = str(root_dir)
@@ -17,15 +17,16 @@ class CommonVoiceDataModule(pl.LightningDataModule):
         self.common_voice_train = CommonVoiceDataset(root_dir, 'train')
         self.common_voice_valid = CommonVoiceDataset(root_dir, 'dev')
         self.common_voice_test = CommonVoiceDataset(root_dir, 'test')
+        self.custom_collate_fn = MyCollator(root_dir)
 
     def train_dataloader(self):
         return DataLoader(self.common_voice_train, batch_size=self.batch_size,
-                          num_workers=self.num_workers, collate_fn=custom_collate_fn, shuffle=True)
+                          num_workers=self.num_workers, collate_fn=self.custom_collate_fn, shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.common_voice_valid, batch_size=self.batch_size,
-                          num_workers=self.num_workers, collate_fn=custom_collate_fn)
+                          num_workers=self.num_workers, collate_fn=self.custom_collate_fn)
 
     def test_dataloader(self):
         return DataLoader(self.common_voice_test, batch_size=self.batch_size,
-                          num_workers=self.num_workers, collate_fn=custom_collate_fn)
+                          num_workers=self.num_workers, collate_fn=self.custom_collate_fn)
