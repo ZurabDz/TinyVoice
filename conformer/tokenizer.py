278ae193ae6ad284a243eb9952ac181666a3a082
ZurabDz <dzindzibadzezurabi@gmail.com>
2025-09-20 17:35:41 -0200

    reworked tokenizer

--- conformer/tokenizer.py
+++ conformer/tokenizer.py
@@ -1,5 +1,6 @@
-import os.path as osp
 import json
+from pathlib import Path
+
 
 class Tokenizer:
     def __init__(self, vocab: list[str]):
@@ -24,16 +25,30 @@ class Tokenizer:
             last_char_id = char_id
         return "".join(decoded_chars)
     
-    def save(self, path):
-        with open(path, 'w', encoding='utf-8') as f:
-            json.dump({'char_to_id': self.char_to_id, 'id_to_char': self.id_to_char, 'blank_id': self.blank_id}, f, ensure_ascii=False)
+    def save(self, path: Path, name: str = 'tokenizer.json') -> None:
+        with open(path / name, 'w', encoding='utf-8') as f:
+            json.dump(
+                {
+                    'char_to_id': self.char_to_id,
+                    'id_to_char': {str(k): v for k, v in self.id_to_char.items()},
+                    'blank_id': self.blank_id
+                },
+                f,
+                ensure_ascii=False
+            )
 
-    def load(self, path):
+
+    @classmethod
+    def load(cls, path):
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
-            self.char_to_id = data['char_to_id']
-            self.id_to_char = data['id_to_char']
-            self.blank_id = data['blank_id']
+            id_to_char = {int(k): v for k, v in data['id_to_char'].items()}
+            vocab = [id_to_char[i] for i in range(len(id_to_char))]
+            tokenizer = cls(vocab)
+            tokenizer.char_to_id = data['char_to_id']
+            tokenizer.id_to_char = id_to_char
+            tokenizer.blank_id = data['blank_id']
+            return tokenizer
 
 
 def build_tokenizer(data):
